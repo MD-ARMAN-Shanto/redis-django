718b0916155bd44eea1764f08b291ab40b3f13bf
arman <hossain.arman@northsouth.edu>
2022-01-02 23:51:25 +0600

    update redis url

--- redis_django_app/redsi_app/urls.py
+++ redis_django_app/redsi_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import view_cached_books, manage_items_redis, manage_item, get_values_redis, delete_from_redis
+from .views import  manage_items_redis, manage_item, get_values_redis, delete_from_redis
 
 app_name = 'redis_api'
 
 urlpatterns = [
-    path('products_cache/', view_cached_books, name="books"),
     path('redis_get_values/', get_values_redis, name="get_redis_items"),
     path('items/', manage_items_redis, name="items"),
     path('redis_single_item/<slug:key>/', manage_item, name="single_item"),
